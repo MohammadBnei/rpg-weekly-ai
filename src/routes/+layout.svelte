2d3d6ccc46085163e6e044455fcfd1b307de4009
MohammadBnei <mohammadamine.banaei@pm.me>
2024-09-07 19:34:14 +0200

    chore(package.json): update dependencies and devDependencies versions refactor(Header.svelte): update class names and remove TvIcon component import feat(Header.svelte): update website name to "RPG Weekly" refactor(+layout.svelte): add Header and Footer components to layout
    refactor(page.svelte): remove unused imports and variables, simplify code structure, and improve readability
    
    feat(weekly page): add functionality to search for recommendations based on user input
    
    This commit adds the following changes to the weekly page:
    - Import necessary modules and components
    - Declare and initialize variables for loading, error, endStream, and makeRecommendation
    - Declare and initialize variables for searchResponse and recommendations
    - Implement a reactive statement to update recommendations based on searchResponse
    - Declare and initialize variables for cinemaType, selectedCategories, and specificDescriptors
    - Implement a search function to fetch recommendations from the server
    - Implement a clearForm function to reset the form
    - Add HTML structure and styling for the page
    - Render the Form component and display recommendations if available
    - Display loading message and error message if applicable
    
    These changes were made to allow users to search for recommendations based on their preferences and display the results on the weekly page.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,14 @@
 	import '../app.css';
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
+	import Header from '$lib/Header.svelte';
+	import Footer from '$lib/Footer.svelte';
 
 	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
-<div class="min-h-screen w-full">
+<div class="w-full min-h-screen">
+	<Header />
 	<slot />
+	<Footer />
 </div>
